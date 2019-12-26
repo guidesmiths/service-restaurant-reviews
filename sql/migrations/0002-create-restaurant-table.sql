5002d09cc182c3a464d7cf0be4143563ae611124
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-26 15:34:29 +0100

    feature: extending base info
    extending db info to test the app
    
    noticket

--- sql/migrations/0002-create-restaurant-table.sql
+++ sql/migrations/0002-create-restaurant-table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE restaurants.restaurant(
   id SERIAL PRIMARY KEY,
   name VARCHAR (30) NOT NULL,
-  address TEXT
+  address TEXT,
+	rate FLOAT,
+	avgprice FLOAT
 );
 
 CREATE INDEX IF NOT EXISTS restaurants_restaurant_id_idx ON restaurants.restaurant(id);
--- sql/migrations/0003-populate-restaurant-table.sql
+++ sql/migrations/0003-populate-restaurant-table.sql
@@ -1,2 +1,2 @@
-INSERT INTO restaurants.restaurant (name, address) VALUES ('Longinos', 'Calle Falsa');
-INSERT INTO restaurants.restaurant (name, address) VALUES ('VIPS', 'Calle Falsa 124');
+INSERT INTO restaurants.restaurant (name, address, rate, avgprice) VALUES ('Longinos', 'Calle Falsa', 7.8, 11.0);
+INSERT INTO restaurants.restaurant (name, address, rate, avgprice) VALUES ('VIPS', 'Calle Falsa 124', 5.4, 10.55);
