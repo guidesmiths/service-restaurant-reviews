8c2e8c7c5584847b4ad00bad5b757493fb784869
Daniel Herrero <danielherrerohernando@gmail.com>
2020-02-18 22:59:29 +0100

    test: updating mock data

--- test/sql/queries/insertmockdata.sql
+++ test/sql/queries/insertmockdata.sql
@@ -1,5 +1,5 @@
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('Longinos', 'Calle Falsa', 7.8, 11.0, 'https://media-cdn.tripadvisor.com/media/photo-s/12/1d/e3/04/20180219-151820-largejpg.jpg');
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('VIPS', 'Calle Falsa 124', 5.4, 10.55, 'https://lh3.googleusercontent.com/proxy/JewTJ5HeY818FoMy7Q-prBD8XvSq70I1wEb26csBEoq6xgeGWVxTJxC9vNgaFjGAIsQyFJJHBvC53ZlNJxnyJnWMvLUjdVOQvhWK6WUmeSHp8Rhfo4glowQZBza0PxzkZq_5zw');
+INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('Longinos', 'Calle Falsa', 7.8, 11.0, 'https://media-cdn.tripadvisor.com/media/photo-s/10/a2/5d/e2/photo0jpg.jpg');
+INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('VIPS', 'Calle Falsa 124', 5.4, 10.55, 'https://marinedacity.com/blog/wp-content/uploads/2015/11/VIPSOK.jpg');
 INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('Goiko Grill', 'Calle Falsa', 7.8, 11.0, 'https://s04.s3c.es/imag/_v0/770x420/1/0/3/490x_goiko-grill-coqueta.jpg');
 INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('New York Burger', 'Calle Falsa 124', 5.4, 10.55, 'https://restauracionnews.com/wp-content/uploads/2018/10/NYBurgerMoraleja.jpg');
 INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('La Mucca', 'Calle Falsa 124', 6.4, 10.55, 'https://grupolamusa.com/media/grupo-la-musa-latina-madrid-4-rincon-lamparas-techo.jpg');
