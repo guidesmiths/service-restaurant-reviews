27c234f15c0c4f7845b2162d8ce3757ff30481c0
Daniel Herrero <danielherrerohernando@gmail.com>
2020-03-17 12:30:50 +0100

    feat: adding sql logic

--- components/store/initStore.js
+++ components/store/initStore.js
@@ -1,12 +1,22 @@
+/* eslint-disable camelcase */
 module.exports = () => {
 	const start = async ({ pg }) => {
 		const getAllRestaurants = async () => {
-			const t = await pg.formattedQuery('all-restaurants');
-			return t.rows;
+			const { rows } = await pg.formattedQuery('select-all-restaurants');
+			return rows;
+		};
+		const getRestaurantReviews = async id => {
+			const rates = (await pg.query('select-restaurant-rates', [id])).rows[0];
+			const reviews = (await pg.query('select-restaurant-reviews', [id])).rows;
+			return [rates, reviews];
+		};
+		const insertReview = async ({ restaurant_id, authorname, authorimg, date, content, rate, cuisinerate, pricerate, settingrate }) => {
+			const { rows } = await pg.query('insert-review', [restaurant_id, authorname, authorimg, date, content, rate, cuisinerate, pricerate, settingrate]);
+			return rows;
 		};
 
 		return {
-			getAllRestaurants,
+			getAllRestaurants, getRestaurantReviews, insertReview,
 		};
 	};
 
