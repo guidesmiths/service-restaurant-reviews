b682550a538087c713036747f71b6f633ff18593
Daniel Herrero <danielherrerohernando@gmail.com>
2020-03-21 18:44:38 +0100

    fix: sql get all restaurant full join

--- sql/queries/select-all-restaurants.sql
+++ sql/queries/select-all-restaurants.sql
@@ -1,5 +1,5 @@
 SELECT restaurant.id, name, avgprice, distance, cuisine, img, avg (rate) AS average_rate
 FROM restaurants.restaurant
-INNER JOIN restaurants.review ON restaurant.id = review.restaurant_id
+FULL JOIN restaurants.review ON restaurant.id = review.restaurant_id
 GROUP BY restaurant.id
 ORDER BY average_rate DESC;
