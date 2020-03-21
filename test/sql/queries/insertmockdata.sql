6af838376a85a2d97c259ed21c31dfc1814013b6
Daniel Herrero <danielherrerohernando@gmail.com>
2020-03-21 18:30:45 +0100

    test

--- test/sql/queries/insertmockdata.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('Longinos', 'Calle Falsa', 7.8, 11.0, 'https://media-cdn.tripadvisor.com/media/photo-s/10/a2/5d/e2/photo0jpg.jpg');
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('VIPS', 'Calle Falsa 124', 5.4, 10.55, 'https://marinedacity.com/blog/wp-content/uploads/2015/11/VIPSOK.jpg');
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('Goiko Grill', 'Calle Falsa', 7.8, 11.0, 'https://s04.s3c.es/imag/_v0/770x420/1/0/3/490x_goiko-grill-coqueta.jpg');
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('New York Burger', 'Calle Falsa 124', 5.4, 10.55, 'https://restauracionnews.com/wp-content/uploads/2018/10/NYBurgerMoraleja.jpg');
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('La Mucca', 'Calle Falsa 124', 6.4, 10.55, 'https://grupolamusa.com/media/grupo-la-musa-latina-madrid-4-rincon-lamparas-techo.jpg');
-INSERT INTO restaurants.restaurant (name, address, rate, avgprice, img) VALUES ('Ramen', 'Calle Falsa 124', 6.2, 10.55, 'https://i2.wp.com/gastroactivity.com/wp-content/uploads/2017/10/Ramen-Shifu-Grupo-Ayala-Japon.jpg?resize=800%2C600&ssl=1');
